366e1fa048c29df0998ab3bf7137675373095627
Brandon Donnelson <branflake2267@gmail.com>
2012-09-01 17:32:12 -0700

    Get rid of the eclipse warning for MapEvent

--- Apis_Google_Maps/src/com/google/gwt/maps/client/events/MapEventFormatter.java
+++ Apis_Google_Maps/src/com/google/gwt/maps/client/events/MapEventFormatter.java
@@ -6,6 +6,6 @@ import com.google.gwt.ajaxloader.client.Properties;
  * interface createEvent in {@link MapHandlerRegistration} using generics
  * @param <E>
  */
-public interface MapEventFormatter<E extends MapEvent> {
+public interface MapEventFormatter<E extends MapEvent<?, E>> {
   public E createEvent(Properties properties);
 }
