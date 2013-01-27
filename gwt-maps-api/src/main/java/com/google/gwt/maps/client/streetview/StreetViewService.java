2410e12c3eab8577c6bec302b3e6e2d24a2bc0e7
Brandon Donnelson <branflake2267@gmail.com>
2013-01-26 19:35:09 -0800

    deprecated comments

--- gwt-maps-api/src/main/java/com/google/gwt/maps/client/streetview/StreetViewService.java
+++ gwt-maps-api/src/main/java/com/google/gwt/maps/client/streetview/StreetViewService.java
@@ -47,7 +47,7 @@ public class StreetViewService extends JavaScriptObject {
 
   /**
    * Use newInstance(). This method has been spelled incorrectly.
-   * Deprecated on 1/26/2013
+   * Deprecated on 1/26/2013. Deprecate in 3.11
    */
   @Deprecated
   public static final StreetViewService newInstnace() {
